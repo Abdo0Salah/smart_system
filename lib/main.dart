8eac69a8f4675501d5c02f149ab08b7475879bef
sohaelmazoon22 <92324578+sohaelmazoon22@users.noreply.github.com>
2023-12-21 17:25:18 +0200

    some changes on home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:smart_system/ui/home.dart';
+
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());

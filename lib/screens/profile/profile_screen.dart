3a1cc16009520c947c3a99f3e6d8ae38ee58506e
SamarTalat <samartalat1412@gmail.com>
2024-04-24 01:45:29 +0200

    add new.

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  static const String routeName = 'profile';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF0F3F7),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40 , left: 20),
+                    child: InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: Color(0xff1B406D),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40 , left: 100),
+                    child: Column(children: [
+                      Text(
+                        'Profile',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff292A2E)
+                        ),
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Image.asset('assets/images/profile/Vector.png'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Student name",
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff2C3E50),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 6 , bottom: 40),
+                    child: Text("20CS1H2103@kristujayanti.com",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xff2C3E50),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Color(0xff2C3E50),
+                        style: BorderStyle.solid,
+                        width: 2.5,
+                      ),
+                      fixedSize: Size(224, 60),
+                      backgroundColor: Color(0xff2C3E50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: (){},
+                    child:
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30),
+                          child: Row(
+                            children: [
+                            Text(
+                              "Edit Profile",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xffD5F4FF),
+                              ),
+                            ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Icon(
+                                  color: Color(0xffD5F4FF),
+                                  size: 28,
+                                  Icons.edit
+                                ),
+                              ),
+                          ],),
+                        ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                    height: 20,
+                  ),
+                  TextButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Color(0xff2C3E50),
+                        style: BorderStyle.solid,
+                        width: 2.5,
+                      ),
+                      fixedSize: Size(224, 60),
+                      backgroundColor: Color(0xff2C3E50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: (){},
+                    child:
+                        Text(
+                          "Back To Home",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xffD5F4FF),
+                          ),
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+    );
+  }
+}

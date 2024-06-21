f28c50bca265b2323afeac6a5868c4502deff53f
Abdelrahman Salah <abdelrahmansalah102@gmail.com>
2024-06-21 22:21:08 +0200

    finish doctor0 by abdo

---
+++ lib/doctor/Abdo/widgets/DoctorMaterialItem.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../model/DoctotMaterialModel.dart';
+
+
+class DoctorMaterialItem extends StatelessWidget {
+  DoctorMaterialMoel doctormaterialModel;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     
+
+      padding: EdgeInsets.all(18),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20)
+          )),
+      child: Column(
+        children: [
+          Expanded(child: Image.asset(doctormaterialModel.image,
+          width: 70,height: 70,)),
+          Text(doctormaterialModel.name,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black)),
+        ],
+      ),
+    );
+  }
+
+  DoctorMaterialItem(this.doctormaterialModel);
+}

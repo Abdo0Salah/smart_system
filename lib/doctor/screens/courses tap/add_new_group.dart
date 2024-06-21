1804aa971a3dda24d05240a65b40ad18b47e7ab0
SamarTalat <samartalat1412@gmail.com>
2024-06-21 18:36:24 +0300

    add new in doctor.

--- lib/doctor/screens/courses tap/add_new_group.dart
+++ lib/doctor/screens/courses tap/add_new_group.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_system/doctor/screens/courses%20tap/add_attachment.dart';
 
-
 class AddNewGroup extends StatelessWidget {
   static const String routeName = 'AddNewGroup';
 
@@ -26,16 +25,16 @@ class AddNewGroup extends StatelessWidget {
               elevation: 10,
               backgroundColor: Color(0xff4C6ED7),
               child: Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(60),
-                      topRight: Radius.circular(60),
-                    ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
                   ),
-                  child:  Image.asset('assets/images/doctor/correct.png'),
+                ),
+                child: Image.asset('assets/images/doctor/correct.png'),
+              ),
             ),
           ),
-        ),
         ),
         body: SingleChildScrollView(
           child: Column(
@@ -43,8 +42,7 @@ class AddNewGroup extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(
-                padding:
-                const EdgeInsets.only(left: 20, top: 30, bottom: 30),
+                padding: const EdgeInsets.only(left: 20, top: 30, bottom: 30),
                 child: Text(
                   'Selected Topic',
                   style: TextStyle(
@@ -54,11 +52,12 @@ class AddNewGroup extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                height: 100,
+                height: 50,
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 15 , bottom: 5),
-                child: Text('Group name',
+                padding: const EdgeInsets.only(left: 15, bottom: 5),
+                child: Text(
+                  'Group name',
                   style: TextStyle(
                     fontSize: 15,
                     fontWeight: FontWeight.w400,
@@ -69,22 +68,120 @@ class AddNewGroup extends StatelessWidget {
                 padding: const EdgeInsets.all(15.0),
                 child: TextFormField(
                     decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(5),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  filled: true,
+                  fillColor: Color(0xffFDFDFD),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xffBDBDBD),
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, bottom: 5, top: 10),
+                child: Text(
+                  'Doctor /Staff ‘s name',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  filled: true,
+                  fillColor: Color(0xffFDFDFD),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xffBDBDBD),
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  hintText: 'Ehab Rousdy',
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, bottom: 5, top: 10),
+                child: Text(
+                  'Description of Group',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 35.0, horizontal: 5.0),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  filled: true,
+                  fillColor: Color(0xffFDFDFD),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xffBDBDBD),
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  hintText:
+                      'موضوعات مختاره /الفرقه التالته /التيرم التاني 2023_2024',
+                )),
+              ),
+              SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.push_pin_outlined,
+                      size: 30,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Pin Group',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 15,
                         ),
-                        filled: true,
-                        fillColor: Color(0xffFDFDFD),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            width: 1,
-                            color: Color(0xffBDBDBD),
-                          ),
-                          borderRadius: BorderRadius.circular(5),
                         ),
-                        )),
+                        SizedBox(height: 10),
+                        Container(
+                          width: 280,
+                          height: 1,
+                          color: Color(0xff554A4A),
+                        ),
+                      ],
+                    ),
+                    Image.asset('assets/images/doctor/line-md_switch-filled.png'),
+                  ],
+                ),
               ),
             ],
           ),

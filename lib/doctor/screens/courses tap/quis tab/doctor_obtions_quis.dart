4945585f3883df06289b80fe8dd92f3569ba0d95
Abdelrahman Salah <abdelrahmansalah102@gmail.com>
2024-06-24 01:09:45 +0200

    finish zefto by abdo

--- lib/doctor/screens/courses tap/quis tab/doctor_obtions_quis.dart
+++ lib/doctor/screens/courses tap/quis tab/doctor_obtions_quis.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../widget/add_option_widget.dart';
 import 'create_quiz.dart';
@@ -27,21 +28,50 @@ class _DoctorObtionsQuisState extends State<DoctorObtionsQuis> {
         elevation: 0,
       ),
       backgroundColor: const Color(0xffEFF3F7FF),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: double.infinity,
+                  height: 35,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Network",
+                      style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                "Questions",
+                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 24),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "what is tcp/ip ?",
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                    color: Colors.grey),
+              ),
+              Container(
                 width: double.infinity,
-                height: 35,
+                height: 1,
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
+                    color: Colors.black, borderRadius: BorderRadius.circular(20)),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
@@ -50,172 +80,146 @@ class _DoctorObtionsQuisState extends State<DoctorObtionsQuis> {
                   ),
                 ),
               ),
-            ),
-            Text(
-              "Questions",
-              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 24),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              "what is tcp/ip ?",
-              style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 18,
-                  color: Colors.grey),
-            ),
-            Container(
-              width: double.infinity,
-              height: 1,
-              decoration: BoxDecoration(
-                  color: Colors.black, borderRadius: BorderRadius.circular(20)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Network",
-                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: double.infinity,
+                height: 35,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10))),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "#Answer Text",
+                        style:
+                            TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+                      ),
+                      Text(
+                        "Correct",
+                        style:
+                            TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Container(
-              width: double.infinity,
-              height: 35,
-              decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10))),
-              child: Padding(
+              Container(
+                height: 200.h,
+                child: Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return   AddOptionWidget();
+                    },
+                    itemCount: numberOfOptions,
+        
+        
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: TextButton
+                  (onPressed: (){
+                    setState(() {
+                      numberOfOptions++;
+                     });
+                    },
+                    child: Text("Add Option",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18,
+                      color: Colors.blue
+                    ),),
+                style: ButtonStyle(
+        
+                  backgroundColor: MaterialStateProperty.all(Colors.white)
+                ),),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                     Text(
+                       "Enter the Degree of the Question:",
+                       style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                     ),
+                     TextFormField(
+                       decoration: InputDecoration(
+                         filled: true,
+                         fillColor: Colors.white,
+                         enabled: true,
+                         enabledBorder: OutlineInputBorder(
+        
+                         ),
+        
+                       ),
+                     ),
+                   ],
+                ),
+              ),
+              Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      "#Answer Text",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
-                    ),
-                    Text(
-                      "Correct",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
-                    ),
+                    Spacer(),
+                    TextButton(onPressed: (){
+                      Navigator.of(context).pop(CreateQuiz.routeName);
+        
+        
+                    },
+                      child: Text("Add New Question",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20,
+        
+                        ),),
+                      style: ButtonStyle(
+        
+                          backgroundColor: MaterialStateProperty.all(Colors.black)
+                      ),),
                   ],
                 ),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return   AddOptionWidget();
-                },
-                itemCount: numberOfOptions,
-
-
-              ),
-            ),
-
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: TextButton
-                (onPressed: (){
-                  setState(() {
-                    numberOfOptions++;
-                   });
-                  },
-                  child: Text("Add Option",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 18,
-                    color: Colors.blue
-                  ),),
-              style: ButtonStyle(
-
-                backgroundColor: MaterialStateProperty.all(Colors.white)
-              ),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                 children: [
-                   Text(
-                     "Enter the Degree of the Question:",
-                     style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-                   ),
-                   TextFormField(
-                     decoration: InputDecoration(
-                       filled: true,
-                       fillColor: Colors.white,
-                       enabled: true,
-                       enabledBorder: OutlineInputBorder(
-
-                       ),
-
-                     ),
-                   ),
-                 ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
+              Row(
                 children: [
-                  Spacer(),
-                  TextButton(onPressed: (){
-                    Navigator.of(context).pop(CreateQuiz.routeName);
-
-
-                  },
-                    child: Text("Add New Question",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 20,
-
-                      ),),
-                    style: ButtonStyle(
-
-                        backgroundColor: MaterialStateProperty.all(Colors.black)
-                    ),),
-                ],
-              ),
-            ),
-
-            Row(
-              children: [
-                Expanded(
-                  child: TextButton(onPressed: (){
-                    Navigator.of(context).pop(CreateQuiz.routeName);
-
-                  },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("Create Quiz",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 18,
-
-                        ),),
-                    ),
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.blue)
+                  Expanded(
+                    child: TextButton(onPressed: (){
+                      Navigator.of(context).pop(CreateQuiz.routeName);
+        
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Create Quiz",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+        
+                          ),),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.blue)
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-
-          ],
+                ],
+              ),
+        
+            ],
+          ),
         ),
       ),
     );

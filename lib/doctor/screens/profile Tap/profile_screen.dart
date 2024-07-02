34543f2a7f7ed04b772cfbac2bc06c99f40dbbcb
sohaelmazoon22 <92324578+sohaelmazoon22@users.noreply.github.com>
2024-07-03 01:54:27 +0300

    editing gradel

--- lib/doctor/screens/profile Tap/profile_screen.dart
+++ lib/doctor/screens/profile Tap/profile_screen.dart
@@ -73,28 +73,32 @@ class DoctorProfile1 extends StatelessWidget {
                         Navigator.of(context).pushNamed(DoctorProfile2.routeName);
                       },
                       child:
-                          Padding(
-                            padding: const EdgeInsets.only(left: 30),
-                            child: Row(
-                              children: [
-                              Text(
+                      Padding(
+                        padding:  EdgeInsets.only(left: 30.w),
+                        child: Row(
+                          children: [
+                            Expanded(flex:2,
+                              child: Text(
                                 "Edit Profile",
                                 style: TextStyle(
-                                  fontSize: 24,
+                                  fontSize: 20.sp,
                                   fontWeight: FontWeight.w500,
-                                  color: Color(0xffD5F4FF),
+                                  color: const Color(0xffD5F4FF),
                                 ),
                               ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 20),
-                                  child: Icon(
-                                    color: Color(0xffD5F4FF),
-                                    size: 28,
+                            ),
+                            Expanded(flex:1,
+                              child: Padding(
+                                padding:  EdgeInsets.only(left: 20.w),
+                                child: Icon(
+                                    color: const Color(0xffD5F4FF),
+                                    size: 28.sp,
                                     Icons.edit
-                                  ),
                                 ),
-                            ],),
-                          ),
+                              ),
+                            ),
+                          ],),
+                      ),
                     ),
                     SizedBox(
                       width: 20,

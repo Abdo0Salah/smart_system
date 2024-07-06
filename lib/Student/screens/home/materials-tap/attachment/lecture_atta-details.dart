16ba601b3040e85455eff107765112d6988c3a6d
sohaelmazoon22 <92324578+sohaelmazoon22@users.noreply.github.com>
2024-07-07 02:47:35 +0300

    editing gradel

--- lib/Student/screens/home/materials-tap/attachment/lecture_atta-details.dart
+++ lib/Student/screens/home/materials-tap/attachment/lecture_atta-details.dart
@@ -1,18 +1,25 @@
+import 'dart:math';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 import '../../../../../apiModels/GetFilesDataOfLecturesAttachmentModel.dart';
 import '../../../../../cubit/user_cubit.dart';
 import '../../../../../cubit/user_state.dart';
-
+import 'package:open_file/open_file.dart';
 class LectureAttavhmentDetails extends StatefulWidget {
-  static const String routeName = 'LectureAttavhmentDetails';
+  static const String routeName = 'LectureAttachmentDetails';
 
   @override
-  _LectureAttavhmentDetailsState createState() => _LectureAttavhmentDetailsState();
+  _LectureAttavhmentDetailsState createState() =>
+      _LectureAttavhmentDetailsState();
 }
 
 class _LectureAttavhmentDetailsState extends State<LectureAttavhmentDetails> {
+  final Dio dio = Dio();
+
   @override
   void initState() {
     super.initState();
@@ -35,7 +42,8 @@ class _LectureAttavhmentDetailsState extends State<LectureAttavhmentDetails> {
             } else if (state is GetFilesDataOfLecturesAttachmentSuccess) {
               return _buildSubjectList(state.FilesDataR);
             } else if (state is GetFilesDataOfLecturesAttachmentFailure) {
-              return Center(child: Text('Failed to load subjects: ${state.errMessage}'));
+              return Center(
+                  child: Text('Failed to load subjects: ${state.errMessage}'));
             } else {
               return Center(child: Text('Unknown state'));
             }
@@ -45,26 +53,32 @@ class _LectureAttavhmentDetailsState extends State<LectureAttavhmentDetails> {
     );
   }
 
-  Widget _buildSubjectList(List<GetfilesdataoflecturesattachmentModel> FilesData) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-      child: GridView.builder(
-        itemCount: FilesData.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.w,
-          mainAxisSpacing: 10.h,
-          childAspectRatio: 0.75,
+  Widget _buildSubjectList(
+      List<GetfilesdataoflecturesattachmentModel> FilesData) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 50.h,
         ),
-        itemBuilder: (context, index) {
-          final fileData = FilesData[index];
-          return _buildSubjectItem(fileData);
-        },
-      ),
+        Expanded(
+          child: GridView.builder(
+            itemCount: FilesData.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (context, index) {
+              final FileData = FilesData[index];
+              return _buildSubjectItem(FileData, index + 1);
+            },
+          ),
+        ),
+      ],
     );
   }
 
-  Widget _buildSubjectItem(GetfilesdataoflecturesattachmentModel fileData) {
+  Widget _buildSubjectItem(GetfilesdataoflecturesattachmentModel FileData, int index) {
     return Padding(
       padding: EdgeInsets.all(8.0).w,
       child: Container(
@@ -83,14 +97,14 @@ class _LectureAttavhmentDetailsState extends State<LectureAttavhmentDetails> {
               ),
               SizedBox(height: 8.h),
               Text(
-                fileData.fileName ?? '-',
+                FileData.fileName ?? '-',
                 style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15.sp),
                 textAlign: TextAlign.center,
                 // overflow: TextOverflow.ellipsis,
               ),
               SizedBox(height: 8.h),
               Text(
-                fileData.description ?? '-',
+                FileData.description ?? '-',
                 style: TextStyle(
                   fontWeight: FontWeight.w400,
                   fontSize: 11.sp,
@@ -104,13 +118,11 @@ class _LectureAttavhmentDetailsState extends State<LectureAttavhmentDetails> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   InkWell(
-                    onTap: () {
-                      // Handle file download
-                    },
+                    onTap: () => _downloadFile(FileData.fileName, FileData.id??0),
                     child: Icon(
                       Icons.download,
                       color: Colors.black,
-                      size: 25,
+                      size: 25.sp,
                     ),
                   ),
                 ],
@@ -120,5 +132,62 @@ class _LectureAttavhmentDetailsState extends State<LectureAttavhmentDetails> {
         ),
       ),
     );
+  }
+
+  Future<void> _downloadFile(String? fileName, int fileId) async {
+    if (fileName == null) return;
+
+    try {
+      Directory? downloadsDir;
+
+      if (Platform.isAndroid) {
+        downloadsDir = await getExternalStorageDirectory();
+        String newPath = "";
+        List<String> paths = downloadsDir!.path.split("/");
+        for (int i = 1; i < paths.length; i++) {
+          String folder = paths[i];
+          if (folder != "Android") {
+            newPath += "/" + folder;
+          } else {
+            break;
+          }
+        }
+        newPath = newPath + "/Download";
+        downloadsDir = Directory(newPath);
+      } else if (Platform.isIOS) {
+        downloadsDir = await getApplicationDocumentsDirectory();
+      }
+      Random random1 = new Random();
+      var count1 = random1.nextInt(20);
+      String filePath = "${downloadsDir!.path}/${count1}_$fileName";
+      Random random = new Random();
+      var count = random.nextInt(20);
+
+      while (await File(filePath).exists()) {
+        filePath = "${downloadsDir.path}/${count}_$fileName";
+        count++;
+      }
+
+      final response = await dio.download(
+        "http://eirpsystem.runasp.net/api/Files/download/lecture/$fileId",
+        filePath,
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('File downloaded successfully!')),
+        );
+        OpenFile.open(filePath);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to download file.')),
+        );
+      }
+    } catch (e) {
+      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error occurred: $e')),
+      );
+    }
   }
 }

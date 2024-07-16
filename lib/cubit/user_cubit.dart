43de8a80b9b010d9e1fce0c2b306e3ccc5b73292
Abdelrahman Salah <abdelrahmansalah102@gmail.com>
2024-07-16 12:39:47 +0200

    11111

---
+++ lib/cubit/user_cubit.dart
@@ -0,0 +1,432 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:smart_system/core/api/end_ponits.dart';
+import 'package:smart_system/cubit/user_state.dart';
+import '../apiModels/GetFilesDataOfLecturesAttachmentModel.dart';
+import '../apiModels/GetSectionsAttachmentModel.dart';
+import '../apiModels/GetfilesdataofSectionsattachmentModel.dart';
+import '../apiModels/OpenAssignmentModel.dart';
+import '../apiModels/getAllStudentAttendanceModel.dart';
+import '../apiModels/getByLevelAndTerm.dart';
+import '../apiModels/get_all_assignments_model.dart';
+import '../apiModels/get_all_meetings_model.dart';
+import '../apiModels/get_all_posts_model.dart';
+import '../apiModels/get_groups_model.dart';
+import '../apiModels/get_lecture_assignment_model.dart';
+import '../apiModels/get_lectures_attachment.dart';
+import '../apiModels/get_section_assignment_model.dart';
+import '../apiModels/sign_in_model.dart';
+import '../apiModels/subjectRegisteration_model.dart';
+import '../cache/cache_helper.dart';
+import '../repositories/user_repository.dart';
+
+class UserCubit extends Cubit<UserState> {
+  UserCubit(this.userRepository) : super(UserInitial());
+
+  final UserRepository userRepository;
+
+  //Sign in Form key
+  GlobalKey<FormState> signInFormKey = GlobalKey();
+//  GlobalObjectKey<FormState> signInFormKey = GlobalObjectKey(Object);
+  //final List<GlobalObjectKey<FormState>> signInFormKey = List.generate(10, (index) => GlobalObjectKey<FormState>(index));
+  TextEditingController signInuserName = TextEditingController();
+  TextEditingController signInPassword = TextEditingController();
+  GlobalKey<FormState> signUpFormKey = GlobalKey();
+  //Profile Pic
+  XFile? profilePic;
+  //Sign up name
+  TextEditingController signUpUserName = TextEditingController();
+  TextEditingController signUpName = TextEditingController();
+  //TextEditingController signUpPhoneNumber = TextEditingController();
+  TextEditingController signUpEmail = TextEditingController();
+  TextEditingController signUpPassword = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
+  TextEditingController signUrule = TextEditingController();
+  TextEditingController signUpPhone = TextEditingController();
+  TextEditingController signUpLevel = TextEditingController();
+  TextEditingController signUpTerm = TextEditingController();
+  TextEditingController signUpGender = TextEditingController();
+  TextEditingController signUpSsn = TextEditingController();
+
+  SignInModel? user;
+  SubjectRegisterationModel? SubjectR;
+
+  final TextEditingController nameController0 = TextEditingController();
+  final TextEditingController emailController0 = TextEditingController();
+  final TextEditingController universityEmailController0 = TextEditingController();
+  final TextEditingController passwordController0 = TextEditingController();
+  final TextEditingController ssnController0 = TextEditingController();
+  final TextEditingController phoneController0 = TextEditingController();
+  final TextEditingController genderController0 = TextEditingController();
+  final TextEditingController levelController0 = TextEditingController();
+  final TextEditingController termController0 = TextEditingController();
+  final TextEditingController parentPhoneController0 = TextEditingController();
+  final TextEditingController parentEmailController0 = TextEditingController();
+  final List<GetCoursesbyLevelAndTermModel> courseR = [];
+  final List<GetGroupsModel> groupR = [];
+  final List<GetAllPostsModel>postR =[];
+  final List<GetfilesdataoflecturesattachmentModel>FilesDataR =[];
+  final List<GetLecturesAttachmentModel> attachmenR=[];
+  final List<GetSectionsAttachmentModel> attachmenSectionR=[];
+  final List<Getfilesdataofsectionsattachmentmodel> FilesDataSectionR=[];
+  final List<GetAllAssignmentsModel> allAssignmentR=[];
+  final List<GetAllLectureAssignmentsModel> lectureAssignmentR=[];
+  final List<GetAllSectionAssignmentsModel> sectionAssignmentR=[];
+  final List<OpenAssignmentModel> openAssignmentR=[];
+  final List<GetAllMeetingsModel> meetingR=[];
+  final List<getAllStudentAttendanceModel> AttendanceR=[];
+
+  uploadProfilePic(XFile image) {
+    profilePic = image;
+    emit(UploadProfilePic());
+  }
+///--------------------------------Student--------------------------------------
+  signUp() async {
+    emit(SignUpLoading());
+    final response = await userRepository.signUp(
+      userName: signUpUserName.text,
+      name: signUpName.text,
+      email: signUpEmail.text,
+      password: signUpPassword.text,
+      confirmPassword: confirmPassword.text,
+      role: signUrule.text,
+      phone: signUpPhone.text,
+      level: int.parse(signUpLevel.text),
+      term: int.parse(signUpTerm.text),
+      gender: signUpGender.text,
+      ssn: signUpSsn.text,
+    );
+    response.fold(
+      (errMessage) => emit(SignUpFailure(errMessage: errMessage)),
+      (signUpModel) => emit(SignUpSuccess()),
+    );
+  }
+
+  signIn() async {
+    emit(SignInLoading());
+    final response = await userRepository.signIn(
+      userName: signInuserName.text,
+      password: signInPassword.text,
+    );
+    response.fold(
+      (errMessage) => emit(SignInFailure(errMessage: errMessage)),
+      (signInModel) => emit(SignInSuccess()),
+    );
+  }
+
+  Future<void> logout() async {
+    emit(UserLogoutLoading());
+    final response = await userRepository.logout();
+    response.fold(
+          (error) => emit(UserLogoutFailure(errMessage: error)),
+          (_) => emit(UserLogoutSuccess()),
+    );
+  }
+
+  Future<void> checkLoginStatus() async {
+    final isLoggedIn = await userRepository.isLoggedIn();
+    if (isLoggedIn) {
+      emit(SignInSuccess());
+    } else {
+      emit(UserInitial());
+    }
+  }
+
+  Future<void> getUserProfile() async {
+    emit(GetUserLoading());
+    final result = await userRepository.getUserProfile();
+    result.fold(
+      (error) {
+        print('Error: $error');
+        emit(GetUserFailure(errMessage: error));
+      },
+      (user) {
+        print('User data received in Cubit: $user');
+        emit(GetUserSuccess(user: user));
+      },
+    );
+  }
+
+  Future<void> updateUserProfile() async {
+    emit(UpdateUserLoading());
+    final result = await userRepository.updateUser(
+      id: CacheHelper().getData(key: ApiKey.id) ?? '',
+      name: nameController0.text,
+      email: emailController0.text,
+      universityEmail: universityEmailController0.text,
+      password: passwordController0.text,
+      ssn: ssnController0.text,
+      phone: phoneController0.text,
+      gender: genderController0.text,
+      level: int.parse(levelController0.text),
+      term: int.parse(termController0.text),
+      parentPhone: parentPhoneController0.text,
+      parentEmail: parentEmailController0.text,
+    );
+    result.fold(
+      (error) => emit(UpdateUserFailure(errMessage: error)),
+      (updatedUser) => emit(UpdateUserSuccess(updatedUser: updatedUser)),
+    );
+  }
+
+  SubjectRegisteration() async {
+    emit(SubjectRegisterationLoading());
+    final response = await userRepository.SubjectRegisteration();
+    print(response.toString());
+    response.fold(
+      (errMessage) => emit(SubjectRegisterationFailure(errMessage: errMessage)),
+      (subjectR) => emit(SubjectRegisterationSuccess(subjectR: subjectR)),
+    );
+  }
+
+  Future<void> registerCourses(List<int> courseIds) async {
+    emit(CourseRegistrationLoading());
+    final response = await userRepository.registerCourses(courseIds);
+    response.fold(
+          (errMessage) => emit(CourseRegistrationFailure(errMessage: errMessage)),
+          (registerCoursesModel) => emit(CourseRegistrationSuccess(
+          registerCoursesModel: registerCoursesModel)),
+    );
+  }
+
+  GetCoursesbyLevelAndTerm() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetCoursesbyLevelAndTerm();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (courseR) {
+        // ** new **
+        this.courseR
+          ..clear()
+          ..addAll(courseR);
+        emit(UserSuccessState());
+      },    );
+  }
+
+  getGroups() async {
+    emit(UserLoadingState());
+    final response = await userRepository.getGroups();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (groupR) {
+        // ** new **
+        this.groupR
+          ..clear()
+          ..addAll(groupR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+  GetAllMeetings() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetAllMeetings();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+            (meetingR) {
+          // ** new **
+          this.meetingR
+            ..clear()
+            ..addAll(meetingR);
+          emit(UserSuccessState());
+            },
+    );
+  }
+
+  ///----------------------------posts---------------------------------///
+  GetAllPosts() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetAllPosts();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserLogoutFailure(errMessage: errMessage)),
+          (postR) {
+        // ** new **
+        this.postR
+          ..clear()
+          ..addAll(postR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+  GetAllReplies() async {
+    emit(GetAllRepliesLoading());
+    final response = await userRepository.GetAllReplies();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(GetAllRepliesFailure(errMessage: errMessage)),
+          (replyR) => emit(GetAllRepliesSuccess(replyR: replyR)),
+    );
+  }
+
+
+  Future<void> addComment(String commentContent, int postId,int userId) async {
+    emit(AddCommentLoading());
+    final response = await userRepository.addComment(
+      commentContent: commentContent,
+      postId: postId,
+      userId: userId,
+    );
+    response.fold(
+          (errMessage) => emit(AddCommentFailure(errMessage: errMessage)),
+          (_) => emit(AddCommentSuccess()),
+    );
+  }
+
+///---------------------------Attachment-------------------///
+  GetLecturesAttachment() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetLecturesAttachment();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (attachmenR) {
+        // ** new **
+        this.attachmenR
+          ..clear()
+          ..addAll(attachmenR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+  GetSectionsAttachment() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetSectionsAttachment();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (attachmenSectionR) {
+        // ** new **
+        this.attachmenSectionR
+          ..clear()
+          ..addAll(attachmenSectionR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+  GetFilesDataOfLecturesAttachment() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetFilesDataOfLecturesAttachment();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (FilesDataR) {
+        // ** new **
+        this.FilesDataR
+          ..clear()
+          ..addAll(FilesDataR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+  GetFilesDataOfSectionsAttachment() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetFilesDataOfSectionsAttachment();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (FilesDataSectionR) {
+        // ** new **
+        this.FilesDataSectionR
+          ..clear()
+          ..addAll(FilesDataSectionR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+
+///--------------------------Assignment----------------------///
+  GetAllAssignments() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetAllAssignments();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (allAssignmentR) {
+        // ** new **
+        this.allAssignmentR
+        ..clear()
+      ..addAll(allAssignmentR);
+    emit(UserSuccessState());
+  },
+    );
+  }
+
+  GetAllLectureAssignments() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetAllLectureAssignments();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (lectureAssignmentR) {
+        // ** new **
+        this.lectureAssignmentR
+          ..clear()
+          ..addAll(lectureAssignmentR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+  GetAllSectionAssignments() async {
+    emit(UserLoadingState());
+    final response = await userRepository.GetAllSectionAssignments();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (sectionAssignmentR) {
+        // ** new **
+        this.sectionAssignmentR
+          ..clear()
+          ..addAll(sectionAssignmentR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+  OpenAssignment() async {
+    emit(UserLoadingState());
+    final response = await userRepository.OpenAssignment();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (openAssignmentR) {
+        // ** new **
+        this.openAssignmentR
+          ..clear()
+          ..addAll(openAssignmentR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+
+
+///--------------------------------Attendance--------------------------------------
+  getAllStudentAttendance() async {
+    emit(UserLoadingState());
+    final response = await userRepository.getAllStudentAttendance();
+    print(response.toString());
+    response.fold(
+          (errMessage) => emit(UserFailureState(errMessage: errMessage)),
+          (AttendanceR) {
+        // ** new **
+        this.AttendanceR
+          ..clear()
+          ..addAll(AttendanceR);
+        emit(UserSuccessState());
+      },
+    );
+  }
+
+
+}
---
+++ lib/cubit/user_state.dart
@@ -0,0 +1,145 @@
+import '../apiModels/Register_Courses_Model.dart';
+import '../apiModels/get_all_replies_model.dart';
+import '../apiModels/subjectRegisteration_model.dart';
+import '../apiModels/update_user_model.dart';
+import '../apiModels/user_model.dart';
+
+class UserState {}
+
+final class UserInitial extends UserState {}
+
+final class UserLoadingState extends UserState {}
+
+final class UserFailureState extends UserState {
+  final String errMessage;
+
+  UserFailureState({required this.errMessage});
+}
+
+final class UserSuccessState extends UserState {}
+
+///--------------------------------Student--------------------------------------
+
+final class UploadProfilePic extends UserState {}
+
+///
+final class UpdateUserLoading extends UserState {}
+
+final class UpdateUserSuccess extends UserState {
+  final UpdateUserModel updatedUser;
+
+  UpdateUserSuccess({required this.updatedUser});
+}
+
+final class UpdateUserFailure extends UserState {
+  final String errMessage;
+
+  UpdateUserFailure({required this.errMessage});
+}
+
+///
+
+final class SubjectRegisterationSuccess extends UserState {
+  final List<SubjectRegisterationModel> subjectR;
+
+  SubjectRegisterationSuccess({required this.subjectR});
+}
+
+final class SubjectRegisterationLoading extends UserState {}
+
+final class SubjectRegisterationFailure extends UserState {
+  final String errMessage;
+
+  SubjectRegisterationFailure({required this.errMessage});
+}
+///
+final class CourseRegistrationSuccess extends UserState {
+  final RegisterCoursesModel registerCoursesModel;
+
+  CourseRegistrationSuccess({required this.registerCoursesModel});
+}
+
+final class CourseRegistrationLoading extends UserState {}
+
+final class CourseRegistrationFailure extends UserState {
+  final String errMessage;
+
+  CourseRegistrationFailure({required this.errMessage});
+}
+
+
+///
+final class GetAllRepliesSuccess extends UserState {
+  final List<GetAllRepliesModel> replyR;
+
+  GetAllRepliesSuccess({required this.replyR});
+}
+
+final class GetAllRepliesLoading extends UserState {}
+
+final class GetAllRepliesFailure extends UserState {
+  final String errMessage;
+
+  GetAllRepliesFailure({required this.errMessage});
+}
+
+///
+final class SignInSuccess extends UserState {}
+
+final class SignInLoading extends UserState {}
+
+final class SignInFailure extends UserState {
+  final String errMessage;
+
+  SignInFailure({required this.errMessage});
+}
+
+///
+final class AddCommentLoading extends UserState {}
+
+final class AddCommentSuccess extends UserState {}
+
+final class AddCommentFailure extends UserState {
+  final String errMessage;
+
+  AddCommentFailure({required this.errMessage});
+}
+
+///
+final class SignUpSuccess extends UserState {}
+
+final class SignUpLoading extends UserState {}
+
+final class SignUpFailure extends UserState {
+  final String errMessage;
+
+  SignUpFailure({required this.errMessage});
+}
+
+///
+final class GetUserSuccess extends UserState {
+  final UserModel user;
+
+  GetUserSuccess({required this.user});
+}
+
+final class GetUserLoading extends UserState {}
+
+final class GetUserFailure extends UserState {
+  final String errMessage;
+
+  GetUserFailure({required this.errMessage});
+}
+
+///
+class UserLogoutLoading extends UserState {}
+
+class UserLogoutSuccess extends UserState {}
+
+class UserLogoutFailure extends UserState {
+  final String errMessage;
+
+  UserLogoutFailure({required this.errMessage});
+}
+
+///
